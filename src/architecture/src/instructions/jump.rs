b3d10eb199789ac9c2a8c80bc58ae45ce5b8e69e
Leah Ferrell <leahferrell03@gmail.com>
2019-07-06 13:11:05 -0500

    implemented jump instructions

--- src/architecture/src/instructions/jump.rs
+++ src/architecture/src/instructions/jump.rs
@@ -6,58 +6,43 @@ pub fn default(state: &mut State, instruction: &Instruction) -> bool {
     false
 }
 
-pub fn carry(state: &mut State, instruction: &Instruction) -> bool {
-    if state.cc.cy == 1 {
+fn condition(state: &mut State, instruction: &Instruction, value: u8, expected: u8) -> bool {
+    if value == expected {
         state.pc = instruction.get_addr() as usize;
+    }else{
+        state.pc += 1;
     }
     false
+}
+
+pub fn carry(state: &mut State, instruction: &Instruction) -> bool {
+    condition(state, instruction, state.cc.cy, 1)
 }
 
 pub fn no_carry(state: &mut State, instruction: &Instruction) -> bool {
-    if state.cc.cy == 0 {
-        state.pc = instruction.get_addr() as usize;
-    }
-    false
+    condition(state, instruction, state.cc.cy, 0)
 }
 
 pub fn zero(state: &mut State, instruction: &Instruction) -> bool {
-    if state.cc.z == 1 {
-        state.pc = instruction.get_addr() as usize;
-    }
-    false
+    condition(state, instruction, state.cc.z, 1)
 }
 
 pub fn no_zero(state: &mut State, instruction: &Instruction) -> bool {
-    if state.cc.z == 0 {
-        state.pc = instruction.get_addr() as usize;
-    }
-    false
+    condition(state, instruction, state.cc.z, 0)
 }
 
 pub fn minus(state: &mut State, instruction: &Instruction) -> bool {
-    if state.cc.s == 1 {
-        state.pc = instruction.get_addr() as usize;
-    }
-    false
+    condition(state, instruction, state.cc.s, 1)
 }
 
 pub fn positive(state: &mut State, instruction: &Instruction) -> bool {
-    if state.cc.s == 0 {
-        state.pc = instruction.get_addr() as usize;
-    }
-    false
+    condition(state, instruction, state.cc.s, 0)
 }
 
 pub fn parity_even(state: &mut State, instruction: &Instruction) -> bool {
-    if state.cc.p == 1 {
-        state.pc = instruction.get_addr() as usize;
-    }
-    false
+    condition(state, instruction, state.cc.p, 1)
 }
 
 pub fn parity_odd(state: &mut State, instruction: &Instruction) -> bool {
-    if state.cc.p == 0 {
-        state.pc = instruction.get_addr() as usize;
-    }
-    false
+    condition(state, instruction, state.cc.p, 0)
 }
