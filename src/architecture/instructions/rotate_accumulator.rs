7ba130586009eb691c8cd1cab3833616f0164054
Leah Ferrell <leahferrell03@gmail.com>
2019-07-13 14:38:58 -0500

    added rotate accumulator instructions

--- src/architecture/instructions/rotate_accumulator.rs
+++ src/architecture/instructions/rotate_accumulator.rs
@@ -7,21 +7,103 @@ use crate::architecture::AddressPtr;
 /// Instructions which rotate the contents of the accumulator.
 
 pub fn rotate_left(state: &mut State, instruction: &Instruction) -> AddressPtr {
-    println!("ERROR: {} has not been implemented!", instruction.to_string());
-    state.pc
+    state.cc.cy = state.a > 127;
+    state.a = state.a.rotate_left(1);
+    state.pc + instruction.size()
 }
 
 pub fn rotate_right(state: &mut State, instruction: &Instruction) -> AddressPtr {
-    println!("ERROR: {} has not been implemented!", instruction.to_string());
-    state.pc
+    state.cc.cy = state.a % 2 == 1;
+    state.a = state.a.rotate_right(1);
+    state.pc + instruction.size()
 }
 
 pub fn rotate_left_through_carry(state: &mut State, instruction: &Instruction) -> AddressPtr {
-    println!("ERROR: {} has not been implemented!", instruction.to_string());
-    state.pc
+    let former_carry = if state.cc.cy {1} else {0};
+    state.cc.cy = state.a > 127;
+
+    state.a = (state.a << 1) + former_carry;
+    state.pc + instruction.size()
 }
 
 pub fn rotate_right_through_carry(state: &mut State, instruction: &Instruction) -> AddressPtr {
-    println!("ERROR: {} has not been implemented!", instruction.to_string());
-    state.pc
+    let former_carry = if state.cc.cy {128} else {0};
+    state.cc.cy = state.a % 2 == 1;
+
+    state.a = (state.a >> 1) + former_carry;
+    state.pc + instruction.size()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::architecture::model::opcodes::OpCode;
+
+    #[test]
+    fn test_rotate_left() {
+        let mut state = State {a:0x16,..Default::default()};
+        let instruction = &Instruction{opcode:OpCode::RLC,..Default::default()};
+
+        let result = rotate_left(&mut state, instruction);
+        assert_eq!(result, 1);
+        assert_eq!(state.cc.cy, false);
+        assert_eq!(state.a, 0x2c);
+    }
+
+    #[test]
+    fn test_rotate_right() {
+        let mut state = State {a:0xf2,..Default::default()};
+        let instruction = &Instruction{opcode:OpCode::RLC,..Default::default()};
+
+        let result = rotate_right(&mut state, instruction);
+        assert_eq!(result, 1);
+        assert_eq!(state.cc.cy, false);
+        assert_eq!(state.a, 0x79);
+    }
+
+    #[test]
+    fn test_rotate_left_w_carry() {
+        let mut state = State {a:0xf2,..Default::default()};
+        let instruction = &Instruction{opcode:OpCode::RLC,..Default::default()};
+
+        let result = rotate_left(&mut state, instruction);
+        assert_eq!(result, 1);
+        assert_eq!(state.cc.cy, true);
+        assert_eq!(state.a, 0xe5);
+    }
+
+    #[test]
+    fn test_rotate_right_w_carry() {
+        let mut state = State {a:0x79,..Default::default()};
+        let instruction = &Instruction{opcode:OpCode::RLC,..Default::default()};
+
+        let result = rotate_right(&mut state, instruction);
+        assert_eq!(result, 1);
+        assert_eq!(state.cc.cy, true);
+        assert_eq!(state.a, 0xbc);
+    }
+
+    #[test]
+    fn test_rotate_left_through_carry() {
+        let mut state = State {a:0xb5,..Default::default()};
+        let instruction = &Instruction{opcode:OpCode::RLC,..Default::default()};
+
+        let result = rotate_left_through_carry(&mut state, instruction);
+        assert_eq!(result, 1);
+        assert_eq!(state.cc.cy, true);
+        assert_eq!(state.a, 0x6a);
+    }
+
+    #[test]
+    fn test_rotate_right_through_carry() {
+        let mut state = State {a:0x6a,..Default::default()};
+        state.cc.cy = true;
+
+        let instruction = &Instruction{opcode:OpCode::RLC,..Default::default()};
+
+        let result = rotate_right_through_carry(&mut state, instruction);
+        assert_eq!(result, 1);
+        assert_eq!(state.cc.cy, false);
+        assert_eq!(state.a, 0xb5);
+    }
 }
--- src/architecture/model/mod.rs
+++ src/architecture/model/mod.rs
@@ -3,29 +3,3 @@ pub mod registers;
 pub mod condition_codes;
 pub mod opcodes;
 pub mod instruction;
-
-type ConditionFlags = [bool];
-
-type Registers = [u8];
-
-enum Reg {
-    A,
-    B,
-    C,
-    D,
-    E,
-    H,
-    L,
-    M
-}
-
-enum Flag {
-    CY,
-    S1,
-    P,
-    S2,
-    AC,
-    S3,
-    Z,
-    S
-}
